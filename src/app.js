6cca6599e03fa5ae735f49e8cb4d48259fdb39db
ranasauravsingh <ranasaurav121@gmail.com>
2025-04-26 13:59:28 +0530

    [Refactor][Main] exploring order of sequences & HTTP methods

--- src/app.js
+++ src/app.js
@@ -2,12 +2,45 @@ const express = require("express");
 
 const app = express();
 
-app?.get("/", (req, res) => {
-    res?.send("Dev Tinder running");
-});
+//? If writing "/", then all routes will be handle by this middleware
+// app?.use("/", (req, res) => {
+//     res?.send("Dev Tinder running");
+// });
 
-app?.use("/test", (req, res) => {
-    res?.send("This is test api route");
+// app?.use("/hello", (req, res) => {
+//     res?.send("hello hello hello");
+// });
+
+// app?.use("/test", (req, res) => {
+//     res?.send("This is test api route");
+// });
+
+
+//? So to "/" must always be the last route, so that all other routes can be handled by their middlewares
+// app?.use("/hello", (req, res) => {
+//     res?.send("hello hello hello");
+// });
+
+// app?.use("/test", (req, res) => {
+//     res?.send("This is test api route");
+// });
+
+// app?.use("/", (req, res) => {
+//     res?.send("Dev Tinder running");
+// });
+
+//? HTTP methods api functions
+app?.get("/users", (req, res) => {
+    res?.send({ message: "Users fetch successfully" });
+});
+app?.post("/users", (req, res) => {
+    res?.send({ message: "User created successfully" });
+});
+app?.delete("/users", (req, res) => {
+    res?.send({ message: "User deleted successfully" });
+});
+app?.patch("/users", (req, res) => {
+    res?.send({ message: "User data updated successfully" });
 });
 
 app?.listen(7777, (req, res) => {
