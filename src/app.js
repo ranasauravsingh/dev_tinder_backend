ff6329d5688d09cae09d0417eae63c25b0de798a
ranasauravsingh <ranasaurav121@gmail.com>
2025-04-28 07:31:37 +0530

    [Configure][Users] data sanitization at api & db level

--- src/app.js
+++ src/app.js
@@ -7,21 +7,59 @@ const app = express();
 app.use(express.json());
 
 //? 1) Create a user
-// app.post("/signup", async (req, res) => {
-// 	const userData = req.body;
+app.post("/signup", async (req, res) => {
+	const userData = req.body;
+	try {
+		const ALLOWED_KEYS = ["firstName", "emailId", "password", "age"];
+		const isValidUserPayload = Object.keys(userData).every((key) =>
+			ALLOWED_KEYS.includes(key)
+		);
 
-// 	try {
-// 		const user = new Users(userData);
-// 		await user.save();
-// 		res.status(200).send({
-// 			message: "User created successfully",
-// 		})
-// 	} catch (err) {
-// 		res.status(400).send({
-// 			message: `Something went wrong: ${err?.message}`,
-// 		});
-// 	}
-// });
+		if (!isValidUserPayload) {
+			return res.status(400).send({
+				message: "Invalid user data",
+			});
+		}
+
+		if (
+			!userData?.firstName ||
+			!userData?.emailId ||
+			!userData?.password ||
+			!userData?.age
+		) {
+			return res.status(400).send({
+				message:
+					"Please provide all required fields [firstName, emailId, password, age]",
+			});
+		}
+
+		const isFirstNameValid =
+			userData?.firstName?.length >= 4 &&
+			userData?.firstName?.length <= 20;
+		if (!isFirstNameValid) {
+			return res.status(400).send({
+				message: "First name should be between 4 to 20 characters",
+			});
+		}
+
+		const isAgeValid = userData?.age >= 18;
+		if (!isAgeValid) {
+			return res.status(400).send({
+				message: "Age should be greater than or equal to 18",
+			});
+		}
+
+		const user = new Users(userData);
+		await user.save();
+		res.status(200).send({
+			message: "User created successfully",
+		});
+	} catch (err) {
+		res.status(400).send({
+			message: `Something went wrong: ${err?.message}`,
+		});
+	}
+});
 
 //? 2) Get all users
 // app.get("/feed", async (req, res) => {
@@ -87,20 +125,66 @@ app.use(express.json());
 // });
 
 //? 6) Update user by ID
-// app.patch("/user", async (req, res) => {
-// 	try {
-// 		const userId = req.body.userId;
-// 		const userData = req.body;
-// 		// const user = await Users.findOneAndUpdate({ _id: userId});
-// 		const user = await Users.findByIdAndUpdate(userId, userData , { returnDocument: "after"});
-// 		console.log(user);
-// 		res.send("User updated successfully");
-// 	} catch (err) {
-// 		res.status(400).send({
-// 			message: `Something went wrong: ${err?.message}`,
-// 		});
-// 	}
-// });
+app.patch("/user/:userId", async (req, res) => {
+	try {
+		const userData = req.body;
+		// const userId = req.body.userId;
+		const userId = req.params?.userId;
+
+		const ALLOWED_KEYS = [
+			"firstName",
+			"lastName",
+			"gender",
+			"photoUrl",
+			"age",
+			"skills",
+		];
+		const isValidUserPayload = Object.keys(userData).every((key) =>
+			ALLOWED_KEYS.includes(key)
+		);
+
+		if (!isValidUserPayload) {
+			return res.status(400).send({
+				message: "Invalid user data",
+			});
+		}
+
+		const isFirstNameValid =
+			userData?.firstName?.length >= 4 &&
+			userData?.firstName?.length <= 20;
+		if (!isFirstNameValid) {
+			return res.status(400).send({
+				message: "First name should be between 4 to 20 characters",
+			});
+		}
+
+		const isLastNameValid = userData?.lastName?.length <= 20;
+		if (!isLastNameValid) {
+			return res.status(400).send({
+				message: "Last name should be lese than 20 characters",
+			});
+		}
+
+		const isAgeValid = userData?.age >= 18;
+		if (!isAgeValid) {
+			return res.status(400).send({
+				message: "Age should be greater than or equal to 18",
+			});
+		}
+
+		// const user = await Users.findOneAndUpdate({ _id: userId});
+		const user = await Users.findByIdAndUpdate(userId, userData, {
+			returnDocument: "after",
+			runValidators: true,
+		});
+		console.log(user);
+		res.send("User updated successfully");
+	} catch (err) {
+		res.status(400).send({
+			message: `Something went wrong: ${err?.message}`,
+		});
+	}
+});
 
 //? 7) Update user by EmailID
 // app.patch("/user", async (req, res) => {
--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,22 +1,74 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-	firstName: {
-		type: String,
+//? Schema Type ["https://mongoosejs.com/docs/schematypes.html"]
+//? DB level validation
+
+const userSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			minlength: 4,
+			maxlength: 20,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			maxlength: 20,
+		},
+		emailId: {
+			type: String,
+			lowercase: true,
+			trim: true,
+			unique: true,
+			required: true,
+			validate(value) {
+				if (!validator?.isEmail(value)) {
+					throw new Error("Email is not valid");
+				}
+			},
+		},
+		password: {
+			type: String,
+			required: true,
+			validate(value) {
+				if (!validator?.isStrongPassword(value)) {
+					throw new Error("Password is not strong enough");
+				}
+			},
+		},
+		age: {
+			type: Number,
+			min: 18,
+			required: true,
+		},
+		gender: {
+			type: String,
+			validate(value) {
+				//? Custom validation
+				if (!["female", "male", "other"].includes(value)) {
+					throw new Error("Gender data is not valid");
+				}
+			},
+		},
+		photoUrl: {
+			type: String,
+			default: "https://www.w3schools.com/howto/img_avatar.png",
+			validate(value) {
+				if (value && !validator?.isURL(value)) {
+					throw new Error("Photo URL is not valid");
+				}
+			},
+		},
+		skills: {
+			type: [String],
+			enum: ["html", "css", "javascript", "react", "nodejs"],
+		},
 	},
-	lastName: {
-		type: String,
-	},
-	emailId: {
-		type: String,
-	},
-	gender: {
-		type: String,
-	},
-	phoneNumber: {
-		type: Number,
-	},
-});
+	{
+		timestamps: true, //? createdAt, updatedAt
+	}
+);
 
 const Users = mongoose.model("Users", userSchema);
 module.exports = Users;
